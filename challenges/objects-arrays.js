d5971a81e81ce0b02370aa270800f1d5b9fa984f
David Kuseh <davegru203@gmail.com>
2019-08-23 07:35:58 +0000

    solved object challenges in objects-arrays.js

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -77,7 +77,7 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
 
-// const universities = [];
+const universities = [];
 
 console.log(universities)
 
